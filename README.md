e5690bccac750270af34a6673e70fd658b4b3ab2
jhung <jhung@ocadu.ca>
2015-07-24 12:15:08 -0400

    FLUID-5610, FLUID-5692: Added mobile styling and dependencies.

--- README.md
+++ README.md
@@ -1,18 +1,50 @@
-# Doc-base Starter Content
+# docs-core Starter Content
 
 This package includes sample content, styles, and navigation which you can
-customize to create your own documentation site.
+customize to create your own documentation site using Docpad.
 
-# How to Use
+# Get Started Making a New / Custom Site
 
-1. Get the doc-base project by running `npm install something`
-2. Get the doc-base-starter project using git:
-`git clone https://the-doc-base-start-url <your directory>`
+1. Clone the `docs-core` project: `git clone https://github.com/jhung/docs-core`
+2. Clone the `docs-template` project into a custom directory: `git clone https://github.com/jhung/docs-template myProject`  (TODO: update this URL)
+3. Change directories into myProject: `cd myProject`
+4. Install DocPad if it isn't already installed: `sudo npm install -g docpad`
+5. Get the required node modules: `npm install` (TODO: package.json looks for `docs-core` is in a sibling directory to `docs-template`. This should be fixed to use an URL for docs-core instead)
+6. Generate the HTML. This will create output in the ./out/ directory: `docpad generate --env static`
+7. Open the index.html located in the ./out/ directory in a web browser to confirm everything is working and to see the placeholder site.
+8. You can now customize the site.
 
-# What is all this stuff?
+# Commiting Changes to Your New Site
 
-- `/src/documents/*.md`  - the articles go here. Written in Markdown. By default populated with some placeholder documents.
-- `/src/documents/css/*.css.styl` - CSS written in Stylus syntax goes in here.
-- `/site-structure.json`  - the structure for navigation. By default has navigation for the placeholder content.
-- `/src/layouts/default.html.handlebars` - the default content template for laying out the documents.
-- `/src/ghpages-files/*`    - the 404 page, 404 styling, and gh-pages CNAME.
+When you are ready to commit changes, you will first need to change the local git repository's origin to match your own repository. We do this by:
+
+1. renaming initial `origin` to `docs-template`
+2. add a new `origin` to reference the new site's repository.
+
+Example:
+```
+git remote rename origin docs-template
+git remote add origin http://www.github.com/~your-project/your-repository/
+git remote -v
+```
+
+Once this is done, you can commit to the new origin for your site.
+
+# Getting Updates
+
+Periodically `docs-template` will have updates. To update:
+
+```
+git fetch docs-template
+git merge docs-template/master
+npm update
+```
+
+# Deploy to GitHub Pages
+
+```
+docpad deploy-ghpages --env static
+```
+
+*Note:* The above command will deploy to the origin of the repository. To deploy
+to production, you may need to be working from Master, not a fork.
---
+++ docpad.js
@@ -0,0 +1,91 @@
+/*
+Copyright 2014 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+
+var path = require("path");
+var fs = require("fs");
+var docBase = require("doc-base");
+
+// The documentation root on GitHub:
+// Used to build URLs for "Edit on GitHub" links
+// var githubDocRoot = "https://github.com/fluid-project/doc-base/blob/master/src/documents/"
+var githubDocRoot = "";
+
+var siteStructure = JSON.parse(fs.readFileSync("site-structure.json"));
+
+// We locate the images within the src/documents directory so that images can
+// be viewed on GitHub, as well as in the DocPad output. We need to
+// instruct DocPad to treat the images specially so that they are not
+// processed. We tell DocPad to ignore the images using "ignorePaths" and we
+// then copy them ourselves with a "writeAfter" event handler.
+var rootPath = process.cwd();
+var imagesSrcDir = path.join(rootPath, "src", "documents", "images");
+var imagesDestDir = "out/images";
+
+module.exports = {
+    rootPath: rootPath,
+    filesPaths: [
+        docBase.getStaticFilesDir(),
+        "static"
+    ],
+    ignorePaths: [ imagesSrcDir ],
+    renderSingleExtensions: true,
+    templateData: {
+        siteStructure: siteStructure
+    },
+    plugins: {
+        handlebars: {
+            helpers: {
+                rewriteMdLinks: docBase.helpers.rewriteMdLinks,
+                getGithubLocation: docBase.helpers.makeGithubLocationHelper(githubDocRoot),
+                getRelativeUrl: docBase.helpers.getRelativeUrl,
+                ifEqual: docBase.helpers.ifEqual
+            }
+        },
+        highlightjs: {
+            aliases: {
+                stylus: "css"
+            }
+        },
+        stylus: {
+            stylusLibraries: {
+                nib: true
+            },
+            stylusOptions: {
+                compress: true,
+                'include css': true
+            }
+        },
+        uglify: {
+            //  Disable UglifyJS on the development environment.
+            environments: {
+                development: {
+                    enabled: false
+                }
+            },
+
+            //  Pass false to skip compressing entirely. Pass an object to specify custom
+            //  compressor options: http://lisperator.net/uglifyjs/compress .
+            compress: {},
+
+            //  Pass false to skip mangling names.
+            mangle: {}
+        }
+    },
+
+    environments: {
+        development: {
+            stylusOptions: {
+                // Disable compression on the development environment
+                compress: false
+            }
+        }
+    }
+};
