2435f3173e5f27a71053a24817be605d6eb32df4
Ned Zimmerman <ned@bight.dev>
2020-10-22 11:16:22 -0300

    fix: update branch references

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,12 +2,12 @@
 Copyright the Fluidic Eleventy copyright holders.
 
 See the AUTHORS.md file at the top-level directory of this distribution and at
-https://github.com/fluid-project/fluidic-11ty/raw/master/AUTHORS.md.
+https://github.com/fluid-project/fluidic-11ty/raw/main/AUTHORS.md.
 
 Licensed under the New BSD license. You may not use this file except in compliance with this License.
 
 You may obtain a copy of the New BSD License at
-https://github.com/fluid-project/fluidic-11ty/raw/master/LICENSE.md.
+https://github.com/fluid-project/fluidic-11ty/raw/main/LICENSE.md.
 */
 
 "use strict";
