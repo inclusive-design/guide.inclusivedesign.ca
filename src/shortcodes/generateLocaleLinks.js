bfbf125bc763fceed996b74e8b84cbb3d9e35abf
Justin Obara <obara.justin@gmail.com>
2021-09-20 13:13:07 -0400

    fix: languge toggle to same card page in different locales

--- src/shortcodes/generateLocaleLinks.js
+++ src/shortcodes/generateLocaleLinks.js
@@ -2,13 +2,13 @@
 
 const config = require("../_data/config.json");
 
-module.exports = (lang, category, collection) => {
+module.exports = (lang, page, category, collection) => {
     let output = "";
     for (let language in config.languages) {
         if (lang && lang !== language) {
             let langProps = config.languages[language];
             if (category && collection) {
-                let matchedPage = collection[`${category}_${language}`].find(page => page.data.eleventyNavigation.key === category);
+                let matchedPage = collection[`${category}_${language}`].find(item => item.fileSlug === page.fileSlug);
                 if (matchedPage) {
                     output += `<a href="${matchedPage.url}">${langProps.name}</a>`;
                 }
