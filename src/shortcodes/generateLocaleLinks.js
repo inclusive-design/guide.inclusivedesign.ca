b752efaa1cfdc6d636968883f1c8f9800eca0011
Justin Obara <obara.justin@gmail.com>
2021-09-20 14:04:59 -0400

    refactor: use transactionKey to find page in alternate locale

--- src/shortcodes/generateLocaleLinks.js
+++ src/shortcodes/generateLocaleLinks.js
@@ -2,13 +2,13 @@
 
 const config = require("../_data/config.json");
 
-module.exports = (lang, page, category, collection) => {
+module.exports = (lang, translationKey, category, collection) => {
     let output = "";
     for (let language in config.languages) {
         if (lang && lang !== language) {
             let langProps = config.languages[language];
             if (category && collection) {
-                let matchedPage = collection[`${category}_${language}`].find(item => item.fileSlug === page.fileSlug);
+                let matchedPage = collection[`${category}_${language}`].find(page => page.fileSlug === translationKey);
                 if (matchedPage) {
                     output += `<a href="${matchedPage.url}">${langProps.name}</a>`;
                 }
