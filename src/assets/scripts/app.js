5713ff81421bab6526b4d4c1a00cb2304fded16a
Justin Obara <obara.justin@gmail.com>
2021-09-30 15:49:19 -0400

    feat: use disclosure widget for mobile nav menu

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,27 +1,5 @@
 "use strict";
 
-// Menu
-jQuery("#menu").menu({
-    position: {my: "right top", at: "right bottom"},
-    icons: {submenu: "ui-icon-blank"},
-    // Remove the anchors from the tabindex as they are triggered
-    // via the select event. (see below)
-    create: function () {
-        jQuery("#menu .ui-menu-item a").attr("tabindex", -1);
-    },
-    // The jQuery menu doesn't seem to like it when the <a> are
-    // specified as the "item" and are nested under an <li>. There
-    // seems to be styling issues and occassionally an error thrown.
-    // The page transition work around below is used to maintian the semantic structure,
-    // while allowing the menu to behave as normal.
-    select: function (event, ui) {
-        var location = ui.item.children("a").attr("href");
-        if (location) {
-            window.location.href = location;
-        }
-    }
-});
-
 const toggleExpansion = (element, state) => {
     let isExpanded = element.getAttribute("aria-expanded") === "true" ? true : false;
     state = typeof(state) !== "undefined" ? state : !isExpanded;
@@ -65,5 +43,4 @@ const menu = (container, button = "button") => {
 };
 
 menu(".idg-language-picker");
-
-
+menu(".idg-mobile-nav");
